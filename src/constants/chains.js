278a365d7a8dc12baf6146c7b8b79c7f1d0c9ecc
Mir Imad Ahmed <mirimadahmed@outlook.com>
2023-12-21 11:09:19 +0800

    init

---
+++ src/constants/chains.js
@@ -0,0 +1,8 @@
+export const networks = [919]
+
+export const ChainId = {
+  MAINNET: 919
+};
+
+export const routerAddress = new Map();
+routerAddress.set(ChainId.MAINNET, "0x3928e186E7a261F303bcC0c2bEfA5f968159b842");
